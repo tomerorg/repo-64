fb413867c2cb111a180233377cd7080c2bfc4dd6
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 13:59:38 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-64",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Python auth 64 with Celery",
+    keywords="repo-64, python",
+    url="",
+)
